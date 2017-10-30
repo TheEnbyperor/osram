470ba3d691028e4286a7f8341174955ead4a08f7
Benjamin Misell <beninjam174@gmail.com>
2017-10-30 10:18:32 +0000

    Get location fix constantly

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class LocationUpdater extends Component {
+    constructor(props) {
+        super(props);
+        this.onFix = props.onFix;
+        this.lostFix =props.lostFix;
+        this.fix = null;
+    }
+
+    componentDidMount() {
+        this.watchId = navigator.geolocation.watchPosition((fix) => {
+            if (this.fix === null) {
+                this.onFix();
+            }
+            this.fix = fix;
+            console.log(fix);
+        }, () => {
+            if (this.fix !== null) {
+                this.lostFix()
+            }
+            this.fix = null;
+        }, {
+            enableHighAccuracy: true,
+            maximumAge: 0
+        })
+    }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchId)
+    }
+
+    render() {
+        return null;
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    hasFix = false;
+
+    render() {
+        return (
+            <div className="App">
+                <LocationUpdater onFix={() => {this.hasFix=true}} lostFix={() => {this.hasFix=false}} />
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;

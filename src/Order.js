58c581d534765d3cfc7e78a01a696b92ab576469
Unknown <beninjam174@gmail.com>
2017-11-19 14:52:51 +0000

    Support unavailable items

--- src/Order.js
+++ src/Order.js
@@ -27,11 +27,11 @@ export default class Order extends Component {
             data.forEach(item => {
                 const itemData = item.val();
                 if (itemData.deleted !== true) {
-                    items.push({ item: item.name, price: item.price, id: val });
+                    items.push({ item: itemData.name, price: itemData.price, extra: { id: item.key, unavailable: itemData.unavailable === true ? true : false } });
                 }
-                self.setState({
-                    items: items
-                });
+            });
+            self.setState({
+                items: items
             });
         });
         database.ref('stalls/' + this.props.id).on('value', (data) => {
@@ -68,7 +68,7 @@ export default class Order extends Component {
     getItem(id) {
         let item = null;
         this.state.items.forEach((val, index) => {
-            if (val.id === id) {
+            if (val.extra.id === id) {
                 item = val;
             }
         });
@@ -133,8 +133,9 @@ export default class Order extends Component {
                             <TableHeader name="item">Item</TableHeader>
                             <TableHeader numeric name="price" cellFormatter={(price) => `${(price / 100).toFixed(2)}`}>
                                 Price</TableHeader>
-                            <TableHeader name="id" cellFormatter={(id) =>
-                                <Button raised colored ripple onClick={this.addItem.bind(this, id)}>Order</Button>}>
+                            <TableHeader name="extra" cellFormatter={(extra) => 
+                                <Button raised colored ripple disabled={extra.unavailable} onClick={this.addItem.bind(this, extra.id)}>Order</Button>
+                            }>
                                 Order</TableHeader>
                         </DataTable>
                     </CardText>
@@ -159,7 +160,7 @@ export default class Order extends Component {
                                 <IconButton name="remove" colored ripple
                                             onClick={this.removeItem.bind(this, id)}>-</IconButton>}/>
                         </DataTable>
-                        Total: £<span>{this.calculateTotal()}</span>
+                        <h6>Total: £<span>{this.calculateTotal()}</span></h6>
                     </CardText>
                     <CardActions border>
                         <Button raised colored ripple onClick={this.placeOrder.bind(this)}>Place Order</Button>
